9a8eca5fa4b89b6d6205902ef06c5bebb5b0d9f2
Gustavo <gustavohs@feevale.br>
2015-11-03 23:59:55 -0200

    Adicionados frameworks.

---
+++ WebContent/res/js/controllers.js
@@ -0,0 +1,19 @@
+var hanasolo = angular.module('hanasolo', []);
+
+hanasolo.controller('hanasoloController', function($scope){
+	
+	
+	
+});
+
+/*
+
+$(function(){
+	$.get('api/CLIENTE?filtro.campo=id_cliente&filtro.condicao=MAIOR&filtro.valor=1&filtro.campo=FATASIA&filtro.condicao=CONTEM&filtro.valor=BEC', 
+		function(data){
+			alert(data);
+	});
+});
+
+*/
+        
